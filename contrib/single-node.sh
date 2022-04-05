1386daea2003b8643cbbe237b548843bf8ec5f97
rene <41963722+renaynay@users.noreply.github.com>
2022-04-05 13:51:26 +0200

    scripts and stuff

--- contrib/single-node.sh
+++ contrib/single-node.sh
@@ -3,21 +3,22 @@
 set -o errexit -o nounset
 
 CHAINID="test"
+app="./celestia-appd"
 
 # Build genesis file incl account for passed address
 coins="1000000000000000uceles"
-celestia-appd init $CHAINID --chain-id $CHAINID 
-celestia-appd keys add validator --keyring-backend="test"
+$app init $CHAINID --chain-id $CHAINID 
+$app keys add validator --keyring-backend="test"
 # this won't work because the some proto types are decalared twice and the logs output to stdout (dependency hell involving iavl)
-celestia-appd add-genesis-account $(celestia-appd keys show validator -a --keyring-backend="test") $coins
-celestia-appd gentx validator 5000000000uceles --keyring-backend="test" --chain-id $CHAINID
-celestia-appd collect-gentxs
+$app add-genesis-account $($app keys show validator -a --keyring-backend="test") $coins
+$app gentx validator 5000000000uceles --keyring-backend="test" --chain-id $CHAINID
+$app collect-gentxs
 
 # Set proper defaults and change ports
-sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
-sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
+sed 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
+sed 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
+sed 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
+sed 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
 
 # Start the celestia-app
-celestia-appd start
+$app start --grpc.enable
